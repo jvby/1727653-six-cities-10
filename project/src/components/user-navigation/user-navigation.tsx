07c059206978aea55f7fb5d7a2df7dfe63179652
jvby <belyaikinv@gmail.com>
2022-08-09 09:05:08 +0300

    еще дополнения

--- project/src/components/user-navigation/user-navigation.tsx
+++ project/src/components/user-navigation/user-navigation.tsx
@@ -1,9 +1,15 @@
+import { AuthorizationStatus } from '../../const';
+import { useAppSelector } from '../../hooks';
 import { store } from '../../store';
 import { logoutAction } from '../../store/api-actions';
 
 export function UserNavigation(): JSX.Element {
+const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+
   return (
     <nav className="header__nav">
+      {authorizationStatus === AuthorizationStatus.Auth?
       <ul className="header__nav-list">
         <li className="header__nav-item user">
           <a className="header__nav-link header__nav-link--profile" href="/#">
@@ -22,6 +28,15 @@ export function UserNavigation(): JSX.Element {
             <span className="header__signout">Sign out</span>
           </a>
         </li>
+        :
+        <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="/#">
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__login">Sign in</span>
+                  </a>
+                  </li>
+                  }
       </ul>
     </nav>
   );
